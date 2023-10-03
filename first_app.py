ef8ead3141f8db29e66fa7b478bc6d39a6408dfe
juliapapp24 <jgp88@cornell.edu>
2023-10-03 14:19:13 -0400

    Finished Streamlit onboarding

--- first_app.py
+++ first_app.py
@@ -2,4 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title(“Julia's title”)
+
+st.set_page_config(
+    page_title="hello",
+    page_icon="👋",
+)
+
+st.title("julespapp")
+st.write("Hi, I'm Jules!")
+
+state_counties = pd.read_csv('state_counties_corrected.csv')
+
+st.sidebar.success("Select a demo above.")
+
+# Select and display only the "FAC_STATE" and "FAC_COUNTY" columns
+selected_columns = ["FAC_STATE", "FAC_COUNTY"]
+selected_data = state_counties[selected_columns]
+st.write(selected_data)
